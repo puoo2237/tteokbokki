b0a1cb28cc7beb707aa325eae698367d8ae4f95e
zmin1021 <zzzzmin0219@gmail.com>
2025-05-09 11:29:05 +0900

    가게 정보 및 메뉴 데이터 DB 코드

--- connect_db.py
+++ connect_db.py
@@ -9,6 +9,8 @@ class ConnectPostgres:
     with open('db_info.json', 'r', encoding='utf-8') as f:
         db_ = json.load(f)
     review_table_name = 'store_review'
+    info_table_name = 'store'
+    menu_table_name = 'store_menu'
 
     def load_engine(self, DB_USER:str, 
                   DB_PASSWORD:str, 
@@ -94,6 +96,96 @@ class ConnectPostgres:
                         "user_feature": ARRAY(TEXT()),
                         "user_hashtag": ARRAY(TEXT()),
                         })
+            print(f"Pandas DataFrame이 성공적으로 저장되었습니다.")
+        except Exception as e:
+            print(f"데이터베이스 저장 오류: {e}")
+        finally:
+            engine.dispose()
+    
+    def create_store_table(self):
+        create_sql = f"""
+                        CREATE TABLE {self.db_['DB_SCHEMA']}.{self.info_table_name} (
+                            store_id int4 NOT NULL,
+                            store_name varchar(50) NOT NULL,
+                            gu varchar(10) NOT NULL,
+                            store_category varchar(10) NULL,
+                            store_score float4 NULL,
+                            store_business_hour _text NULL,
+                            store_location varchar(150) NOT NULL,
+                            store_visit_review_cnt float4 NOT NULL,
+                            store_blog_review_cnt float4 NOT NULL,
+                            store_url text NOT NULL,
+                            CONSTRAINT store_id_pk PRIMARY KEY (store_id)
+                        );
+                    """
+
+        try:
+            engine = self.load_engine(**self.db_)
+            with engine.connect() as connection:
+                with connection.begin() as transaction:
+                    connection.execute(text(create_sql))
+                    connection.commit()
+                    print(f"{self.info_table_name}, 테이블 생성")
+
+        except Exception as e:
+            print(f"오류 발생: {e}")
+    
+    def create_store_menu_table(self):
+        create_sql = f"""
+                        CREATE TABLE {self.db_['DB_SCHEMA']}.{self.info_table_name} (
+                            store_id int4 NOT NULL,
+                            is_tab int2 NOT NULL,
+                            take_out varchar(10) NOT NULL,
+                            menu_name varchar(50) NOT NULL,
+                            menu_description text NULL,
+                            menu_price varchar(20) NULL,
+                            img_path text NULL,
+                            CONSTRAINT store_menu_pk PRIMARY KEY (store_id, is_tab, take_out, menu_name)
+                            CONSTRAINT store_menu_fk FOREIGN KEY (store_id) REFERENCES {self.db_['DB_SCHEMA']}.store(store_id) ON DELETE CASCADE
+                            );
+                    """
+
+        try:
+            engine = self.load_engine(**self.db_)
+            with engine.connect() as connection:
+                with connection.begin() as transaction:
+                    connection.execute(text(create_sql))
+                    connection.commit()
+                    print(f"{self.menu_table_name}, 테이블 생성")
+
+        except Exception as e:
+            print(f"오류 발생: {e}")
+
+    def save_store_info(self, df:pd.DataFrame):
+        """
+        postgres에 store_info 저장하기
+        """
+        engine = self.load_engine(**self.db_)
+        try:
+            df.to_sql(self.info_table_name, 
+                    engine, 
+                    if_exists='append', 
+                    index=False,
+                    dtype={
+                        "store_business_hour": ARRAY(TEXT()),
+                        })
+            print(f"Pandas DataFrame이 성공적으로 저장되었습니다.")
+        except Exception as e:
+            print(f"데이터베이스 저장 오류: {e}")
+        finally:
+            engine.dispose()
+    
+    def save_store_menu(self, df:pd.DataFrame):
+        """
+        postgres에 store_menu 저장하기
+        """
+        engine = self.load_engine(**self.db_)
+        try:
+            df.to_sql(self.menu_table_name, 
+                    engine, 
+                    if_exists='append', 
+                    index=False,
+                    )
             print(f"Pandas DataFrame이 성공적으로 저장되었습니다.")
         except Exception as e:
             print(f"데이터베이스 저장 오류: {e}")
